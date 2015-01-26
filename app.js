ace08085c3e255b642656a9c65cabdc2f400d9e4
npeltier <peltier.nicolas@gmail.com>
2015-01-27 00:06:57 +0100

    first login draft

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 // db
 var dbConnect = process.env.DB.toString();
@@ -17,9 +17,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
 	console.log('db connection opened');
 	var app = express();
-
 	app.set('port', process.env.PORT || 3000);
-
 	// view engine setup
 	app.set('views', path.join(__dirname, 'views'));
 	app.set('view engine', 'jade');
@@ -33,43 +31,17 @@ db.once('open', function (callback) {
 	app.use(require('less-middleware')(path.join(__dirname, 'public')));
 	app.use(express.static(path.join(__dirname, 'public')));
 
-	app.use('/', routes);
-	app.use('/users', users);
 
-	// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-		var err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});
-
-	// error handlers
-
-	// development error handler
-	// will print stacktrace
-	if (app.get('env') === 'development') {
-		app.use(function(err, req, res, next) {
-			res.status(err.status || 500);
-			res.render('error', {
-				message: err.message,
-				error: err
-			});
-		});
-	}
-
-	// production error handler
-	// no stacktraces leaked to user
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: {}
-		});
-	});
+	// required for passport
+	require('./lib/passport')(app, passport);
+	app.use(session({ secret: 'k123.?&=KL-nqsd@éx;D0)941sam§' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash());
+	require('./lib/routes')(app, passport);
 
 	var server = app.listen(app.get('port'), function() {
 	  console.log('Express server listening on port ' + server.address().port);
 	});
-
 	module.exports = app;
 });
