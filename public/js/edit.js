178620234a93870f5c62875d4133b9c3fe7a2f47
Nicolas Peltier <peltier.nicolas@gmail.com>
2015-06-29 23:47:46 +0200

    saves first draft (#14)

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,1 +1,10 @@
 $(".text").attr("contenteditable","true");
+$(".text").blur(function(evt){
+	var data = {};
+	data[$(this).data("name")] = $(this).html().replace("<br>","");
+	$.ajax({
+  		method: "POST",
+  		url: document.location.pathname,
+  		data: data
+	});
+});
