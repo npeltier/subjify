19a6845c69fceb56b915b6dacc8641fa545ba1d8
npeltier <npeltier@adobe.com>
2016-10-22 09:42:08 +0200

    first subjy view

--- models/Subjy.js
+++ models/Subjy.js
@@ -14,4 +14,26 @@ var Subjy = mongoose.model('Subjy', {
 	}]
 });
 
+Subjy.getSample = function(){
+	return {
+		"subject": {s:"free-will","t":"Free Will"},
+		"title": "Free will is our engine",
+		"date": "2012-02-12",
+		"tags": ["philosophy","happiness","morality","good vs evil", "determinism","structuralism"],
+		"author": {"s":"francois", "t":"François"},
+		"blocks": [{
+			"t":"there are nice people, there are bad people"
+		},{
+			"t":"but basically at some point everybody has the choice to do the right thing"
+		},{
+			"t":"you are free, this is a great power, but this comes with great responsabilities"
+		}],
+		"inspiration":{
+			"subject": {"s":"michael-jackson","t":"Michael Jackson"},
+			"author": {"slug":"tom", "t":"Tom"},
+			"title": "\"Who's bad\" is the best song ever written"
+		}
+	};
+}
+
 module.exports = Subjy;
